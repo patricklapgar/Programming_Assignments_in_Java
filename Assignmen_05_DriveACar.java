e7b34ed880eccf7d6271cc1693c91114162c9aaa
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2019-02-21 23:55:38 -0600

    Add files via upload
    Implements multiple method calls inside the 'main' method to list different steps on how to drive a car.

---
+++ Assignmen_05_DriveACar.java
@@ -0,0 +1,45 @@
+/* 
+ @author Patrick Apgar
+ 		 3073052
+ 
+ COSC-1436 Spring 2019
+ Week 5
+ Assignment 05
+ */
+
+public class Assignmen_05_DriveACar {
+//	Below is an ordered list of methods I've written that will display 
+//	the step-by-step process of driving a car.
+	public static void stepOne(String str) {
+		System.out.println(str);
+	}
+	
+	public static void stepTwo(String secondStr) {
+		System.out.println(secondStr);
+	}
+	
+	public static void stepThree(String thirdStr) {
+		System.out.println(thirdStr);
+	}
+	
+	public static void stepFour(String fourthStr) {
+		System.out.println(fourthStr);
+	}
+
+	public static void stepFive(String fifthStr) {
+		System.out.println(fifthStr);
+	}
+	
+	public static void stepSix(String sixthStr) {
+		System.out.println(sixthStr);
+	}
+	
+	public static void main(String [] args) {
+		stepOne("Find your car");
+		stepTwo("Unlock your car");
+		stepThree("Strap seat belt, check mirrors");
+		stepFour("Give address to GPS");
+		stepFive("Start car's engine");
+		stepSix("Drive to destination...");
+	}
+}
